ba0f059f57f64ee23dbc7968c1f506bda7727160
CoderRK <107743164+rajankumar4723@users.noreply.github.com>
2024-06-08 13:57:13 +0530

    fix error

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(
     cors({
       origin: [process.env.FRONTEND_URL],
       methods: ["GET", "POST", "PUT", "DELETE"],
-      credentials: true,
+      // credentials: true,
     })
   );
 
