b40d1dd0966a9ed37984bff4b14db17ce9773985
CoderRK <107743164+rajankumar4723@users.noreply.github.com>
2024-06-08 22:42:05 +0530

    NEW FIX

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.use(
     cors({
       origin: [process.env.FRONTEND_URL],
       methods: ["GET", "POST", "PUT", "DELETE"],
+      withCredentials: true,
       // credentials: true,
     })
   );
